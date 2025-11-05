d1803231b09f2689a3896574151a51ef8e474062
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-05 21:33:10 +0530

    added conversation

---
+++ 04-userpromptvar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo " $PERSON1 : hey $PERSON2 how are you?"
+echo " $PERSON2 : hi $PERSON1 i am fine thanks what about you?"
+echo " $PERSON1 : I am also doing good $PERSON2 when will we meet?"
+echo " $PERSON2 : Its not possible $PERSON1 i am going to india once i come back will meet"
+
+echo " Enter Your Pin Number: "
+read PIN
+echo "your Pin number is: $PIN"
+
