0c1264a3a13bb4be519242cb7cea7ef4406a1e72
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-05 21:19:41 +0530

    added conversation

--- Hello-wold.sh
+++ Hello-wold.sh
@@ -3,4 +3,4 @@
 echo " Trump : hey Musk how are you?"
 echo " Musk: : hi Trump i am fine thanks what about you?"
 echo " Trump : I am also doing good Musk when will we meet?"
-echo " Musk: : No Trump i am going to india once i come back will meet"
+echo " Musk: : no Trump i am going to india once i come back will meet"
