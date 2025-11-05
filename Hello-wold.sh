da95ec0b872c4de95af443993f132c666c5c4ffb
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-05 21:18:18 +0530

    added conversation

--- Hello-wold.sh
+++ Hello-wold.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+echo " Trump : hey Musk how are you?"
+echo " Musk: : hi Trump i am fine thanks what about you?"
+echo " Trump : I am also doing good Musk when will we meet?"
+echo " Musk: : No Trump i am going to india once i come back will meet"
