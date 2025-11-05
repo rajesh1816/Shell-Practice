407c1cdd556d6affa940e9a0987181af13dca4a6
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-05 21:29:51 +0530

    added conversation

---
+++ 03-Dynamicvar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo " $PERSON1 : hey $PERSON2 how are you?"
+echo " $PERSON2 : hi $PERSON1 i am fine thanks what about you?"
+echo " $PERSON1 : I am also doing good $PERSON2 when will we meet?"
+echo " $PERSON2 : Its not possible $PERSON1 i am going to india once i come back will meet"
