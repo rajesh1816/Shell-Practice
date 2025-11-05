f8dabdf324c32b30c36c2dab9b15e92d6e0d9672
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-05 21:28:23 +0530

    added conversation

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,6 +4,6 @@ PERSON1=Trump
 PERSON2=Musk
 
 echo " $PERSON1 : hey $PERSON2 how are you?"
-echo " $ERSON2: : hi $PERSON1 i am fine thanks what about you?"
-echo " $ERSON1 : I am also doing good $PERSON2 when will we meet?"
-echo " $PERSON2: : Its not possible $PERSON1 i am going to india once i come back will meet"
+echo " $PERSON2 : hi $PERSON1 i am fine thanks what about you?"
+echo " $PERSON1 : I am also doing good $PERSON2 when will we meet?"
+echo " $PERSON2 : Its not possible $PERSON1 i am going to india once i come back will meet"
