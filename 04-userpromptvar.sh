8ac8bf0335c0f598f9c7d5ed4ec27d5ee5bd8fd3
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-05 21:37:21 +0530

    added conversation

--- 04-userpromptvar.sh
+++ 04-userpromptvar.sh
@@ -9,6 +9,6 @@ echo " $PERSON1 : I am also doing good $PERSON2 when will we meet?"
 echo " $PERSON2 : Its not possible $PERSON1 i am going to india once i come back will meet"
 
 echo " Enter Your Pin Number: "
-read PIN
+read -s PIN
 echo "your Pin number is: $PIN"
 
