6a23f8ca9bd5c82efbc41a1f7c6a468c32c87355
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-05 21:23:01 +0530

    added conversation

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1 = Trump
+PERSON2 = Musk
+
+echo " Trump : hey Musk how are you?"
+echo " Musk: : hi Trump i am fine thanks what about you?"
+echo " Trump : I am also doing good Musk when will we meet?"
+echo " Musk: : Its not possible Trump i am going to india once i come back will meet"
