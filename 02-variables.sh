30ee3f2555764ea0f2968f816e6d4d28528f867d
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-05 21:24:31 +0530

    added conversation

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,7 @@
 PERSON1 = Trump
 PERSON2 = Musk
 
-echo " Trump : hey Musk how are you?"
-echo " Musk: : hi Trump i am fine thanks what about you?"
-echo " Trump : I am also doing good Musk when will we meet?"
-echo " Musk: : Its not possible Trump i am going to india once i come back will meet"
+echo " PERSON1 : hey PERSON2 how are you?"
+echo " PERSON2: : hi PERSON1 i am fine thanks what about you?"
+echo " PERSON1 : I am also doing good PERSON2 when will we meet?"
+echo " PERSON2: : Its not possible PERSON1 i am going to india once i come back will meet"
