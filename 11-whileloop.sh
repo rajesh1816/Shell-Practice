32255886ff1d62583093ee6ca2e5857e4f49ef33
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-12 20:44:41 +0530

    added content

--- 11-whileloop.sh
+++ 11-whileloop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-while IFS= read -r line
+# if we want to read a file line by line use while loop
+
+while IFS= read line # here line is a variable
 do
      echo $line
 done < 12-set.sh
